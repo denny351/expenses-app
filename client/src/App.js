1594363359fb5d597199ce8a12ce9cda4ea222e8
Denny Park <denny.park93@gmail.com>
2018-03-10 09:59:14 -0800

    get all expenses from api and display on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import ExpensesList from './components/expensesList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	state = {
+		expenses: []
+	};
+
+	componentDidMount() {
+		this.getAllExpenses();
+	}
+
+	getAllExpenses = () => {
+		axios.get('/api/expenses').then(res => {
+			this.setState({ expenses: res.data });
+		});
+  };
+  
+	render() {
+		return (
+			<div className="appContainer">
+				<h1>Expense Tracker</h1>
+				<input
+					className="search"
+					type="text"
+					placeholder="Search here"
+				/>
+				<form className="newForm" onSubmit={this.submitForm}>
+					<input type="text" placeholder="Name" />
+					<input type="text" placeholder="Description" />
+					<input type="number" placeholder="Price" />
+					<button type="submit">Add</button>
+				</form>
+        <ExpensesList expenses={this.state.expenses}/>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ client/src/components/expensesList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const expensesList = (props) => {
+
+  const mapExpenses = props.expenses.map((item, i) => (
+    <div style={{maxWidth:400}}>
+      <div>
+        <span><strong>Name: </strong>{item.name}</span>
+        <span style={{marginLeft:30}}><strong>Price: </strong>{item.value}</span>
+      </div>
+      <div>
+        <strong>Description: </strong>{item.description}
+      </div>
+    </div>
+  ))
+
+  return (
+    <div>
+      {mapExpenses}
+    </div>
+  )
+}
+
+export default expensesList
