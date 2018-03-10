cd9608c1df292110f4c16ce0105c2c2e29f4a34e
Denny Park <denny.park93@gmail.com>
2018-03-09 20:25:13 -0800

    add get all route

--- server/routes/entries.js
+++ server/routes/entries.js
@@ -2,16 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (knex) => {
+
   router.post('/', (req, res) => {
     const entryData = {
       name: req.body.name,
       description: req.body.description,
       value: req.body.value
     }
+    knex('expenses').insert(entryData).then(() => {
+      res.sendStatus(200)
+    })
+  })
 
-    knex('expenses').insert(entryData, '*')
-    .then(doc => {
-      res.send(doc)
+  router.get('/', (req, res) => {
+    knex('expenses').select().then(doc => {
+      res.json(doc)
     })
   })
 
